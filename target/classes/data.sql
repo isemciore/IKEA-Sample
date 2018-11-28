00763561335ec2cedf63490042fe2669bbb1369a
Zhang <erik_zhang11@hotmail.com>
2018-11-28 20:11:50 +0100

    gitignore update

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-insert into customer (id, name, hidden) values (1000, 'Erik', false)
-insert into shopping_list(id, name, customer_id) values (2000, 'Kitchen', 1000)
-insert into item values (3000, '123-543-22', 'Fork')
-insert into item values (3001, '123-543-99', 'Spoon')
-
-
-insert into shopping_list_item (shopping_list_id, item_id, amount) values (2000, 3000, 10)
